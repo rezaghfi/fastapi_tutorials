ec1e7da28ea1483d9a1ec65e92cbea10d60bd271
reza gholamalitabar <reza.gh.fi@gmail.com>
2024-04-16 13:13:39 +0330

    add path parameter basic

--- 2_path_parameters/sample.py
+++ 2_path_parameters/sample.py
@@ -5,8 +5,8 @@ app = FastAPI()
 async def index():
    return {"message": "Hello World"}
 @app.get("/hello/{name}")
-async def hello(name):
-   return {"name": name}
+async def hello(name: str):
+   return {f"hello my lazy {name} you are my best friend:)"}
 
 if __name__ == "__main__":
    uvicorn.run('sample:app', host="0.0.0.0", port=8000, reload=True)
